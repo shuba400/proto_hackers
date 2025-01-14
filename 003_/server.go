543a8f458589827f24c53d5491e49a87f1658f18
Shubhang Singh <51315650+shuba400@users.noreply.github.com>
2025-01-15 01:53:32 +0530

    003 pass

--- 003_/server.go
+++ 003_/server.go
@@ -4,35 +4,98 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"math"
 	"net"
 )
 
+type transaction struct {
+	time []int
+	cost []int
+}
 
 func get_int(arr []byte) int {
 	ans := 0
-	val := 1
-	for i := 3; i > 0; i--{
-		ans += val*int(arr[i])
-		val *= 256
+	tmp := 0
+	ptr := 0
+	for i := 3; i >= 0; i--{
+		for j := 0; j < 8; j++{
+			tmp = ((int(arr[i])>>j)&1)
+			if ptr == 31 {
+				ans -= (tmp * (1<<ptr))
+			} else { 
+				ans += (tmp * (1<<ptr))
+			}
+			ptr++
+		}
 	}
-	ans -= val*int(arr[0])
 	return ans
 
 }
+
+func get_byte(num int) []byte {
+	res := make([]byte, 4)
+	ptr := 31
+	tmp := 0
+	for i := 0; i < 4; i++{
+		tmp = 0
+		for j := 7; j >= 0; j--{
+			if(ptr == 31){
+				if(num < 0){
+					num += (1<<ptr)
+					tmp += 1<<j
+				} 
+			} else {
+				if (num>>ptr)&1 == 1 { 
+					tmp += (1<<j)
+				}
+			}
+			ptr--
+		}
+		res[i] = byte(tmp)
+	}
+	return res
+}
+
+func process_query(res []byte, conn net.Conn, trans *transaction) {
+	a := get_int(res[1:5])
+	b := get_int(res[5:9])
+	q := int(res[0])
+	if q == 73 {
+		trans.time = append(trans.time, a)
+		trans.cost = append(trans.cost, b)
+		return
+	}
+	tot := 0.0
+	val := 0.0
+	for i := 0; i < len(trans.time); i++ {
+		if trans.time[i] >= a && trans.time[i] <= b {
+			val++
+		}
+	}
+	for i := 0; i < len(trans.time); i++ {
+		if trans.time[i] >= a && trans.time[i] <= b {
+			tot += float64(trans.cost[i])/val
+		}
+	}
+	mean := int(math.Trunc(tot))
+	fmt.Printf("Mean is %d",mean)
+	conn.Write(get_byte(mean))
+	return
+}
+
 func handle_connection(conn net.Conn) {
 	defer conn.Close()
 	return_buffer := *&bytes.Buffer{}
-	p := make([]byte,9)
+	p := make([]byte, 9)
 	fmt.Printf("Got connection from %s\n", conn.RemoteAddr())
 	tmp_buffer := make([]byte, 1024)
+	trans := new(transaction)
 	for {
-		new_tansaction()
 		n, err := conn.Read(tmp_buffer)
-		fmt.Print(tmp_buffer[:n])
 		return_buffer.Write(tmp_buffer[:n])
 		for return_buffer.Len() >= 9 {
 			return_buffer.Read(p)
-			handle_transaction(p)
+			process_query(p, conn, trans)
 		}
 		// We should not check for network packet size since they are quite inconsistent
 		// and it is not a gurantee that if packet size is less than buffer size then client
@@ -41,9 +104,7 @@ func handle_connection(conn net.Conn) {
 			break
 		}
 	}
-
-	fmt.Printf("We are senting back data to %s - %d\n", conn.RemoteAddr(), return_buffer.Len())
-	conn.Write(return_buffer.Bytes())
+	fmt.Printf("Closing connection %d", conn.RemoteAddr())
 }
 
 func main() {
@@ -65,9 +126,9 @@ func main() {
 }
 
 /*
-73 0 0 48 57 0 0 0 101 
-73 0 0 48 58 0 0 0 102 
-73 0 0 48 59 0 0 0 100 
-73 0 0 160 0 0 0 0 5 
+73 0 0 48 57 0 0 0 101
+73 0 0 48 58 0 0 0 102
+73 0 0 48 59 0 0 0 100
+73 0 0 160 0 0 0 0 5
 81 0 0 48 0 0 0 64 0
 */
